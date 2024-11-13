fa3fe81e61a6ba3d055fea5544451d100f4a5bc9
mohaa24 <msmathnan@gmail.com>
2024-11-13 20:39:39 +0530

    initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,80 @@
-import Image from "next/image";
+'use client'
+import { useState } from "react";
 import styles from "./page.module.css";
 
 export default function Home() {
+  const [searchString, setSearchString] = useState("jack");
+  const [data, setData] = useState(null);
+
+  const getData = async (string) => {
+    try {
+      const url = ` https://itunes.apple.com/search?term=${string}&entity=musicVideo`;
+      const response = await fetch(url);
+      const dataJSON = await response.json();
+      setData(dataJSON);
+    } catch (error) {
+      console.error(response.status);
+    }
+  };
+
+  const onSearch = () => {
+    getData(searchString);
+  };
+
+  const onType = (string) => {
+    setSearchString(string);
+  };
+
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+        <h1 className="">iTunes Search</h1>
+        <div className={styles.searchContainer}>
+          <input
+            type="text"
+            value={searchString}
+            placeholder="search here.."
+            onChange={(event) => {
+              onType(event.target.value);
+            }}
+          />
+          <button
+            onClick={() => {
+              onSearch();
+            }}
+          >
+            {" "}
+            Search
+          </button>
+        </div>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+        <div className={styles.resultsContainer}>
+          <div className={styles.results}>
+            {data &&
+              data.results &&
+              data.results.map((item) => {
+                return (
+                  <div className={styles.cardContainer}>
+                    <div className={styles.thumbnail}>
+                      {/* <img src={item.artworkUrl60} /> */}
+                    </div>
+                    <div className={styles.info}>
+                      <div className={styles.infoItem}>
+                        Track Name :{item.trackName}
+                      </div>
+                      <div className={styles.infoItem}>
+                        Artist :{item.artistName}
+                      </div>
+                      <div className={styles.infoItem}>
+                        Album :{item.collectionName}
+                      </div>
+                    </div>
+                  </div>
+                );
+              })}
+          </div>
         </div>
       </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
