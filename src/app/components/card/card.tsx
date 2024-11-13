688abb3e1978a1881d02eadf78d740fc44233db2
mohaa24 <msmathnan@gmail.com>
2024-11-13 23:01:35 +0530

    final commit

--- src/app/components/card/card.tsx
+++ src/app/components/card/card.tsx
@@ -7,6 +7,13 @@ type TProps = {
 }
 const Card = ({item}:TProps):JSX.Element=>{
 
+    const shortenString = (string: string, maxLength: number) => {
+      if (string.length <= maxLength) {
+        return string; // Return the string as-is if it's within the limit
+      }
+      return string.substring(0, maxLength) + "..."; // Truncate and append "..."
+    };
+
     return (
       <div className={styles.cardContainer} key={item.trackId}>
         <div className={styles.thumbnail}>
@@ -16,9 +23,15 @@ const Card = ({item}:TProps):JSX.Element=>{
           ></div>
         </div>
         <div className={styles.info}>
-          <div className={styles.infoItem}>Track Name :{item.trackName?item.trackName: ' ---'}</div>
-          <div className={styles.infoItem}>Artist :{item.artistName ? item.artistName: ' ---'}</div>
-          <div className={styles.infoItem}>Album :{item.collectionName ? item.collectionName: ' ---'}</div>
+          <div className={styles.infoItem}>
+            Track Name :{item.trackName ? ` ${shortenString(item.trackName,25)}` : " ---"}
+          </div>
+          <div className={styles.infoItem}>
+            Artist :{item.artistName ? ` ${shortenString(item.artistName,25)}` : " ---"}
+          </div>
+          <div className={styles.infoItem}>
+            Album :{item.collectionName ? ` ${shortenString(item.collectionName,35)}` : " ---"}
+          </div>
         </div>
       </div>
     );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -45,7 +45,6 @@ export default function Home() {
   };
 
 
-  
   const onSearch = () => {
     getData(searchString);
     setPageState("loading");
